8a269a4bd314776f5e5f4bbcdc9b795ff037f4c6
jona-corbaxhi <jonacorbaxhi@gmail.com>
2025-04-22 01:33:00 -0400

    Function input and save expenses

--- expense_tracker.py
+++ expense_tracker.py
@@ -1,4 +1,7 @@
 import sqlite3
+from datetime import datetime
+
+# SQLite Database and expenses table
 
 def init_db():
     conn = sqlite3.connect('expenses.db')
@@ -14,7 +17,23 @@ def init_db():
     ''')
     conn.commit()
     conn.close()
+    
+# Func to input and save expenses
+
+def add_expense():
+    amount = float(input("Amount ($): "))
+    category = input("Category: ")
+    date = input("Date (YYYY-MM-DD, Enter for today): ") or datetime.today().strftime('%Y-%m-%d')
+    notes = input("Reminder (opt): ")
+
+    conn = sqlite3.connect('expenses.db')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO expenses (amount, category, date, notes) VALUES (?, ?, ?, ?)",
+                   (amount, category, date, notes))
+    conn.commit()
+    conn.close()
+    print("Expense added.")
 
 if __name__ == "__main__":
     init_db()
-    print(" Database initialized.")
+    add_expense()
